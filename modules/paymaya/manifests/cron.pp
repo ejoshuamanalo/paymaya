8d88af41abacf3ec41e4f1530da7e59de4121217
Joshua Manalo <josh@server.(none)>
2021-02-16 21:23:44 +0800

    change cron user

--- modules/paymaya/manifests/cron.pp
+++ modules/paymaya/manifests/cron.pp
@@ -4,7 +4,8 @@ class paymaya::cron (
 {
 	cron { 'mem_check':
 		command => "/bin/bash ${src_dir}/my_memory_check",
+		user => 'monitor',
 		minute => '*/10',
-		require => Class[paymaya::src]
+		require => [Class[paymaya::src],Class[paymaya::users]]
 	}
 }
